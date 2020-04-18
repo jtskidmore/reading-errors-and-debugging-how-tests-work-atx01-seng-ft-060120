aeb50d1db4268025c9190ed9da4480c51c699625
JT Skidmore <johntoddskidmore@gmail.com>
2020-04-18 18:15:17 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,13 @@
 # Add your variables here
+
+first_number = 2;
+
+second_number = 4;
+
+product = first_number * second_number;
+
+sum = first_number + second_number;
+
+difference = first_number - second_number;
+
+quotient = first_number / second_number;
